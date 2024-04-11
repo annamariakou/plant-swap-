858c35c3fee9ac6859a30736edb403d379079a78
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-04-12 00:16:10 +0100

    working on nav bar

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 
 import "./Header.css";
 import Image from "next/image";
@@ -5,8 +8,24 @@ import Image from "next/image";
 import Logo from "/Images/Logo.png"
 import Person from "/Images/person_FILL0_wght400_GRAD0_opsz24.svg";
 import Notification from "/Images/notifications_FILL0_wght400_GRAD0_opsz24.svg";
+import Hamburger from "/Images/hamburger.svg"
+import CloseMenu from "/Images/close_menu.svg"
+
+
 
 export default function Header() {
+
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const toggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const handleLinkClick = () => {
+    setNavbarOpen(false);
+  }
+
+
     return <header> 
 <Image
       src={Logo}
@@ -16,12 +35,24 @@ export default function Header() {
       className="logo"
     />
     <nav className="navigation">
+
+    <button className="mobile-menu-toggle" onClick={toggle}>
+    <Image className="hamburgerImg" src={Hamburger} alt="navopen"></Image>
+          </button>
+          {navbarOpen ? (
+          <div className="menu-open">
+              <button onClick={toggle}>
+                <Image className="hamburgerImgClosed" src={CloseMenu} alt="navclosed"></Image>
+              </button>
+
 <ul className="mainNav" >
   <li>HOME</li>
   <li>SWAP</li>
   <li>ABOUT</li>
   <li>GET IN TOUCH</li>
 </ul>
+</div>
+          ) : null}
     </nav>
 <div className="navIcons">
     <Image src={Person} alt="Your SVG" />  
