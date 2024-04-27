c6ea45dc262957536bb58485019ea2ded3fbd752
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-04-27 14:46:52 +0100

    nav bar edits

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -6,59 +6,54 @@ import "./Header.css";
 import Image from "next/image";
 
 import Logo from "/Images/Logo.png"
-import Person from "/Images/person_FILL0_wght400_GRAD0_opsz24.svg";
-import Notification from "/Images/notifications_FILL0_wght400_GRAD0_opsz24.svg";
-import Hamburger from "/Images/hamburger.svg"
-import CloseMenu from "/Images/close_menu.svg"
+import Person from "/Images/user.png";
+import Notification from "/Images/notifications.png";
+import Hamburger from "/Images/hamburger.png"
+import CloseMenu from "/Images/close.png"
+
 
 
 
 export default function Header() {
 
-  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-  const toggle = () => {
-    setNavbarOpen(!navbarOpen);
+  const toggleActiveClass = () => {
+    setIsActive(!isActive);
   };
 
-  const handleLinkClick = () => {
-    setNavbarOpen(false);
-  }
+  return (
+    <header> 
+      <Image
+        src={Logo}
+        width={80}
+        height={80}
+        alt="Plant Swap Logo"
+        className="logo"
+      />
+      <nav className="navigation">
+        <button className="menu" onClick={toggleActiveClass} >
+          {isActive ? (
+            <Image className={`hamburgerImgClosed ${isActive ? 'active' : ''}`} src={CloseMenu} alt="navclosed"></Image>  
+          ):(
+            <Image className="hamburgerImg" src={Hamburger} alt="navopen"></Image>
+          )}
+        </button> 
 
+        <div className={`mainNav ${isActive ? 'active' : ''}`}>
+          <ul>
+            <li>HOME</li>
+            <li>SWAP</li>
+            <li>ABOUT</li>
+            <li>GET IN TOUCH</li>
+          </ul>
+        </div>
 
-    return <header> 
-<Image
-      src={Logo}
-      width={80}
-      height={80}
-      alt="Plant Swap Logo"
-      className="logo"
-    />
-    <nav className="navigation">
-
-    <button className="mobile-menu-toggle" onClick={toggle}>
-    <Image className="hamburgerImg" src={Hamburger} alt="navopen"></Image>
-          </button>
-          {navbarOpen ? (
-          <div className="menu-open">
-              <button onClick={toggle}>
-                <Image className="hamburgerImgClosed" src={CloseMenu} alt="navclosed"></Image>
-              </button>
-
-<ul className="mainNav" >
-  <li>HOME</li>
-  <li>SWAP</li>
-  <li>ABOUT</li>
-  <li>GET IN TOUCH</li>
-</ul>
-</div>
-          ) : null}
-    </nav>
-<div className="navIcons">
-    <Image src={Person} alt="Your SVG" />  
-    <Image src={Notification} alt="Your SVG" />  
-    </div>
-    </header>;
-
-  }
-  
+      </nav>
+      <div className="navIcons">
+        <Image src={Person} alt="Your SVG" />  
+        <Image src={Notification} alt="Your SVG" />  
+      </div>
+    </header>
+  ); 
+}
